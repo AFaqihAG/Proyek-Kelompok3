d3d277b278d6a4d5ae5d6cfb8c2c8a3606ec7ef1
AFaqihAG <105690129+AFaqihAG@users.noreply.github.com>
2022-05-18 20:01:29 +0700

    Argc check and minor text improvements

--- Main.c
+++ Main.c
@@ -9,7 +9,13 @@ int main( int argc, char *argv[] )
     char login[20] = "Kelompok3@1234abcd";
     char akun[20];
 	
-	//writing username/password
+    //Check number of arguments
+    if (argc != 3) {
+        printf("Input: 'File eksekusi' 'Username' 'Password'");
+        return EXIT_FAILURE;
+    }
+
+	//Writing username/password
     fpw = fopen("database/login.bin", "wb");
     fwrite(login, sizeof(char), sizeof(login)/sizeof(char), fpw);
     fclose(fpw);
@@ -36,22 +42,22 @@ int main( int argc, char *argv[] )
         string[ctrl] = strtok(NULL, "@");
     }
 
+    //Getting input username/password
     strcpy(username, string[0]);
     strcpy(password, string[1]);
-
     strcpy(username_input, argv[1]);
     strcpy(password_input, argv[2]);	
-	
-    if( (strcmp(username, username_input) == 0) && (strcmp(password, password_input) == 0) )
-	    {
-	    printf("selamat anda berhasil login");
-	    }
-	    else
-	    {
-	    printf("anda gagal login");
-	    }
+
+    //Check input username & password
+    if( (strcmp(username, username_input) == 0) && (strcmp(password, password_input) == 0)) {
+	    printf("Selamat anda berhasil login\n");
+	} else {
+	    printf("Username/password anda tidak sesuai!\n");
+        printf("Input: 'File eksekusi' 'Username' 'Password'");
+        return EXIT_FAILURE;
+	}
 
 	
 	printf("Anda menjawab %d pertanyaan dengan benar", nilai);
-    return 0;
+    return EXIT_SUCCESS;
 }
