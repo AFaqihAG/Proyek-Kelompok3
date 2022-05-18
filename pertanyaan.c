5fb909897869788e9a5c44a9ca60d38d86b1c867
rizkanawalull <105701042+rizkanawalull@users.noreply.github.com>
2022-05-18 22:38:33 +0700

    Update pertanyaan.c

--- pertanyaan.c
+++ pertanyaan.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(void)
-{
-    char soal;
-    char jawaban[0];
-    
-    
-    
+  
 printf("|+---------------------------------------+|\n");
 printf("|  1. Apakah ibukota negara indonesia?    |\n");
 printf("|+---------------------------------------+|\n");
@@ -18,19 +8,10 @@ printf("|  c.Jakarta            |d.Singapura      |\n");
 printf("|+---------------------------------------+|\n");
 
 printf("jawaban anda [A/B/C/D] :  ");
-scanf("%s", &jawaban[0]);
- if ("jawaban[0] --'C' || jawaban[0] --'c'" )
- {
-     printf("\nJawaban Anda Benar\n\n");
-     
- }
- else
- {
-     printf("\ngame over\n\n");
- }
+
 
 printf("|+---------------------------------------+|\n");
-printf("|  1. Apakah lambang negara indonesia?    |\n");
+printf("|  2. Apakah lambang negara indonesia?    |\n");
 printf("|+---------------------------------------+|\n");
 printf("|  a.Elang           |b.Garuda            |\n");
 printf("|+---------------------------------------+|\n");
@@ -38,18 +19,9 @@ printf("|  c.kepala banteng  |d.pohon beringin    |\n");
 printf("|+---------------------------------------+|\n");
 
 printf("jawaban anda [A/B/C/D] :  ");
-scanf("%s", &jawaban[0]);
- if ("jawaban[0] =='B' || jawaban[0] =='b' ")
- {
-  printf("\nJawaban Anda Benar\n\n");
- }
- else
- {
-    printf("\ngame over\n\n");
- }
 
 printf("|+-----------------------------------------+|\n");
-printf("| 1. Apakah warna bendera negara indonesia? |\n");
+printf("| 3. Apakah warna bendera negara indonesia? |\n");
 printf("|+-----------------------------------------+|\n");
 printf("|  a.Putih biru           |b.Merah putih    |\n");
 printf("|+-----------------------------------------+|\n");
@@ -57,18 +29,10 @@ printf("|  c.Putih merah          |d.Merah biru     |\n");
 printf("|+-----------------------------------------+|\n");
 
 printf("jawaban anda [A/B/C/D] :  ");
-scanf("%s", &jawaban[0]);
- if ("jawaban[0] --'B' || jawaban[0] --'b' ")
- {
-    printf("\nJawaban Anda Benar\n\n");
- }
- else
- {
-    printf("\ngame over\n\n");
- }
+
 
 printf("|+---------------------------------------+|\n");
-printf("| 1.Siapa presiden ke-7 negara indonesia? |\n");
+printf("| 4.Siapa presiden ke-7 negara indonesia? |\n");
 printf("|+---------------------------------------+|\n");
 printf("|  a.Jokowi dodo        |b.SBY            |\n");
 printf("|+---------------------------------------+|\n");
@@ -76,18 +40,10 @@ printf("|  c.Soekarno           |d.Megawati       |\n");
 printf("|+---------------------------------------+|\n");
 
 printf("jawaban anda [A/B/C/D] :  ");
-scanf("%s", &jawaban[0]);
- if ("jawaban[0] --'A' || jawaban[0] --'a' ")
- {
-    printf("\nJawaban Anda Benar\n\n");
- }
- else
- {
-    printf("\ngame over\n\n");
- }
+
 
 printf("|+-------------------------------------------+|\n");
-printf("| 1.Tahun berapakah negara indonesia merdeka? |\n");
+printf("| 5.Tahun berapakah negara indonesia merdeka? |\n");
 printf("|+-------------------------------------------+|\n");
 printf("|  a.1954           |b.1944                   |\n");
 printf("|+-------------------------------------------+|\n");
@@ -95,16 +51,4 @@ printf("|  c.1955           |d.1945                   |\n");
 printf("|+-------------------------------------------+|\n");
 
 printf("jawaban anda [A/B/C/D] :  ");
-scanf("%s", &jawaban[0]);
- if ("jawaban[0] --' D' || jawaban[0] --'d' ")
- {
-    printf("\nJawaban Anda Benar\n\n");
- }
- else
- {
-    printf("\ngame over\n\n");
- }
 
- return 0;
-  
-}
